680d42ec0a13de3986fa6bf7a350908d4b20fea0
stoias <pddybrd.s@gmail.com>
2017-08-01 21:33:38 +0900

    コミットテスト

--- app/src/main/java/com/example/testapp001/MainActivity.java
+++ app/src/main/java/com/example/testapp001/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.testapp001;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ボタンを設置
+        Button button = (Button)findViewById(R.id.button);
+
+        // テキストの設置
+        textView = (TextView)findViewById(R.id.text_view);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(flag){
+                    textView.setText("TRUE");
+                    flag = false;
+                }else{
+                    textView.setText("FLASE");
+                    flag = true;
+                }
+            }
+        });
+
     }
+
 }
